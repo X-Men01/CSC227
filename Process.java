1f74c03b6b3c8d0cad6b67f5c60bbcb61c6f983d
Ahmed <a7mdixhd@gmail.com>
2023-11-16 14:46:52 +0300

    A

--- Process.java
+++ Process.java
@@ -1,3 +1,12 @@
 public class Process {
-    
+    private int ProcessID , BurstTime , Memory ;
+    private State state;
+    public Process(int processId, int memoryRequired, int burstTime) {
+	    this.ProcessID = processId;
+	    this.Memory = memoryRequired;
+	    this.BurstTime = burstTime;
+	    
+        this.state = State.Waiting;
+	  }
+
 }
---
+++ State.java
@@ -0,0 +1,3 @@
+public enum State {
+    New, Ready, Running, Waiting, Terminated
+}
