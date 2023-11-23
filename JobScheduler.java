bcaa5e4913584df240a3df8822d836ebc3ace47f
YS-AJ <124058670+YS-AJ@users.noreply.github.com>
2023-11-23 14:51:20 +0300

    Update JobScheduler.java

--- JobScheduler.java
+++ JobScheduler.java
@@ -4,36 +4,41 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class JobScheduler extends Thread {
+	
     static ArrayList<Process> jobQueue = new ArrayList<>();
-
     static int nOfTotalJobs = 0;
-
+    int lineNum = 0;
+    
+    int processId, burstTime, memoryRequired ;
+    
     // Read process information from file in a separate thread
-    int lineNum = 0 ;
     public void run() {
 
         try {
             // Reading process
-            Scanner scanner = new Scanner(new FileReader("job.txt"));
+            Scanner scanner = new Scanner(new FileReader("C:\\Users\\yazee\\OneDrive\\Desktop\\New Text Document.txt" ) );
             try {
                 while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
+                	String line = scanner.nextLine();
 					lineNum++;
 					
 					if (lineNum % 2 == 0) {
 						line = line.replaceAll("[^0-9]", " ");
+						
 						 String[] parts = line.split("  ");
-						 int processId = Integer.parseInt(parts[0]);
-						 int burstTime = Integer.parseInt(parts[1]);
-						 int memoryRequired = Integer.parseInt(parts[2]);
-                        
+						 processId = Integer.parseInt(parts[0]);
+						 burstTime = Integer.parseInt(parts[1]);
+						 memoryRequired = Integer.parseInt(parts[2]);
 							synchronized (jobQueue) {
 								jobQueue.add(new Process(processId, burstTime, memoryRequired));
 								nOfTotalJobs++;
 							}
-                    }
-                }
-            } catch (NumberFormatException e) {
+							
+					}
+
+				}
+
+			} catch (NumberFormatException e) {
                 e.printStackTrace();
             }
             
