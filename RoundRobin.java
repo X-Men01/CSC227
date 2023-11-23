b21ddbbce510cf7b7401810950a7feae34a2d6a0
YS-AJ <124058670+YS-AJ@users.noreply.github.com>
2023-11-23 14:49:12 +0300

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -1,98 +1,84 @@
 import java.util.ArrayList;
 
-public class RoundRobin {
-    static ArrayList<Process>finalQueue = new ArrayList<>();
+public class RoundRobin extends Algorithm {
 
-    static ArrayList<GranttChart>granttChart = new ArrayList<>();
+	public RoundRobin(ArrayList<Process> readyQueue, int q) {
 
-    static int Time =0;
+		int quantum = q;
 
-    public RoundRobin(ArrayList<Process> readyQueue, int q) {
+		/*
+		 * if (readyQueue.isEmpty()) {
+		 * System.out.println("\n\n\t\treadyQueue is Empty\n\n"); return; }
+		 */
 
-        int quantum = q;
+		do {
 
-        if (readyQueue.isEmpty()) {
-            System.out.println("\n\n\t\treadyQueue is Empty\n\n");
-            return;
-        }
+			if (!readyQueue.isEmpty())
+				roundrobin(readyQueue, q);
 
-        while (!JobScheduler.jobQueue.isEmpty()) {
+			else {
+				try {
+					Main.Thread2.join(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 
-            // At each loop it will wait for 100ms for Thread2 in case ready queue load more
-            // processes from the job queue
-            try {
-                Main.Thread2.join(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            // if (!readyQueue.isEmpty())
+			}
+		} while (!JobScheduler.jobQueue.isEmpty());
 
-                while (!readyQueue.isEmpty()) {
-                    Process p = readyQueue.remove(0);
+		// At each loop it will wait for 10ms for Thread2 in case ready queue load more
+		// processes from the job queue
 
-                    int RR_Time = p.getBurstTime();
+		loadResults();
 
-                    // If the process doesn't finish ready queue will load it again in the back of
-                    // the queue for the next round
-                    if (RR_Time> quantum) {
-                        Time += quantum;
-                        RR_Time -= quantum;
-                        GranttChart cG = new GranttChart("P" + p.getProcessID(), Time - quantum, Time);
-                        granttChart.add(cG);
+		System.exit(0);
+	}
 
-                        readyQueue.add(p);
+	public static void roundrobin(ArrayList<Process> readyQueue, int q) {
+		int quantum = q;
 
-                    } else {
-                        Time += RR_Time;
-                        p.setTurnAround( Time - p.getArrivalTime()); 
-                        p.setWaitTime((p.getTerminationTime() - p.getProcessID())+p.getWaitTime()); 
-                        p.setTerminationTime(Time);
-                        p.setState(State.Terminated);
-                        System.out.println("Process " + p.getProcessID() + " State: " + p.getState() + " Terminated at: "
-                                + p.getTerminationTime());
-                        GranttChart cG = new GranttChart("P" + p.getProcessID(), Time - p.getBurstTime(), Time);
+		while (!readyQueue.isEmpty()) {
+			Process p = readyQueue.remove(0);
 
-                        granttChart.add(cG);
-                        
-                        finalQueue.add(p);
+			// If the process doesn't finish ready queue will load it again in the back of
+			// the queue for the next round
 
-                        // After terminating the process it will go out the ready queue and in this case
-                        // more memory will be available to be occupied
-                        Memory.releaseMemory(p.getMemory());
-                    }
+			if (p.getBurstTime() > quantum) {
 
-                }
+				Time += quantum;
 
-        } 
+				GranttChart cG = new GranttChart("P" + p.getProcessID(), Time - quantum, Time);
+				granttChart.add(cG);
 
-        loadResults();
+				p.setWaitTime(p.getWaitTime() + (Time - quantum - p.getTerminationTime()));
+				p.setBurstTime(p.getBurstTime() - quantum);
+				p.setTerminationTime(Time);
 
-        System.exit(0);
-    }
-    public static void loadResults(){
-    	//Print the Grantt chart 
-        GranttChart.Table(granttChart);
-        
-        double sumWait=0,sumTurn=0;
-        
-        
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("\tProcess Id\t\tMemory Required\t\tArrival Time\t\t  Burst Time\t\tWaiting Time\t\tTurnAround Time\t\tTermination Time");
-        
+				readyQueue.add(p);
 
-        for (Process p : finalQueue) {
-        	System.out.println("\t"+p.getProcessID()+"               \t\t"+p.getMemory()+"\t\t      "+p.getArrivalTime()+"         \t\t"+p.getBurstTime()+"\t\t         "+p.getWaitTime()+"       \t\t"+p.getTurnAround()+"             \t\t"+p.getTerminationTime());
-        	
-            sumWait+=p.getWaitTime();
-            sumTurn+=p.getTurnAround();
-        }
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+			} else {
 
+				Time += p.getBurstTime();
+				p.setTurnAround(Time - p.getArrivalTime());
 
-        double avgWait =  sumWait/finalQueue.size();
-        double avgTurn =  sumTurn/finalQueue.size();
+				p.setWaitTime(p.getWaitTime() + (Time - p.getBurstTime() - p.getTerminationTime()));
 
-        System.out.println("AVG Waiting: "+avgWait+"\nAVG TrunAround: "+avgTurn);
-    }
+				p.setTerminationTime(Time);
+				p.setState(States.Terminated);
+
+				System.out.println("Process " + p.getProcessID() + " State: " + p.getState() + " Terminated at: "
+						+ p.getTerminationTime());
+
+				GranttChart cG = new GranttChart("P" + p.getProcessID(), Time - p.getBurstTime(), Time);
+				granttChart.add(cG);
+
+				finalQueue.add(p);
+
+				// After terminating the process it will go out the ready queue and in this case
+				// more memory will be available to be occupied
+				Memory.releaseMemory(p.getMemory());
+			}
+		}
+	}
 
 }
