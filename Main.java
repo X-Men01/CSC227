f054b689178d47ef2eb24454fa5a6bfe947de3c5
YS-AJ <124058670+YS-AJ@users.noreply.github.com>
2023-11-23 14:52:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,63 +1,45 @@
 
-
 import java.util.Scanner;
+
 public class Main {
-	static JobScheduler Thread1 =new JobScheduler();
-	static CPUScheduler Thread2 =new CPUScheduler();
+	static JobScheduler Thread1 = new JobScheduler();
+	static CPUScheduler Thread2 = new CPUScheduler();
 
-
-
-
-
-	public static void main (String[] args)   {
-
+	public static void main(String[] args) {
 
 		Thread1.start();
 		Thread2.start();
+
 		try {
 			Thread1.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
 		Scanner input = new Scanner(System.in);
-
-
-
 
 		System.out.println("Choose CPU scheduling algorithm (type num) : ");
 		System.out.println("1.FCFS");
-		System.out.println("2.MQ");
-		System.out.println("3.Round-Robin");
+		System.out.println("2.SJF");
+		System.out.println("3.Round-Robin with q = 3");
+		System.out.println("4.Round-Robin with q = 5");
 		int n = input.nextInt();
-		switch(n) {
-			case 1:
-                FCFS f = new FCFS(CPUScheduler.readyQueue);
-				break;
-			case 2:
-				// Algorithms.MQ(FileLoadJob.readyQueue);
-				break;
-			case 3:
-				// Algorithms.RoundRobin(FileLoadJob.readyQueue);;
-				break;
+
+		switch (n) {
+		case 1:
+			FCFS f = new FCFS(CPUScheduler.readyQueue);
+			break;
+		case 2:
+			SJF s = new SJF(CPUScheduler.readyQueue);
+			break;
+		case 3:
+			RoundRobin rr = new RoundRobin(CPUScheduler.readyQueue, 3);
+			break;
+		case 4:
+			RoundRobin rr2 = new RoundRobin(CPUScheduler.readyQueue, 5);
 		}
-
-
+		
 		System.exit(0);
-
-
-
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
